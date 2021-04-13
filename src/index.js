31863e43272f92bafcc8e82f57883eb752fe42a1
ARSAN <arsandev@users.noreply.github.com>
2021-04-13 22:24:43 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = process.env.WS_PORT || process.env.PORT
+
+const server = app.listen(port, () => {
+  console.log(`server running on :${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send("yuky ws server is running")
+})
+
+
+var io = require('socket.io')(server, {
+  cors: {
+    origin: process.env.WS_CORS_ORIGIN
+  }
+})
+
+io.on('connect', (socket)=>{
+  socket.on('ws', (data) => {
+    /*
+    data {
+      channel,
+      data
+    }
+    */
+    io.emit(data.channel, data.data)
+  })
+})
